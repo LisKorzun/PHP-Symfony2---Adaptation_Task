80626e35d7de833022dc0e58b693ed72a241419d
Olesya Korzun <liskorzun@gmail.com>
2015-03-19 18:39:34 +0300

    Fix mistakes

--- src/TestLab/CommonBundle/Controller/AbstractController.php
+++ src/TestLab/CommonBundle/Controller/AbstractController.php
@@ -3,17 +3,29 @@
 namespace TestLab\CommonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 /**
  * @Route(service="testlab.common.controller.abstract")
  */
 abstract class AbstractController
 {
-    protected $templating;
+    private  $templating;
 
-    public function __construct(EngineInterface $templating)
+    /**
+     * Set templating
+     * @param EngineInterface $templating
+     */
+    public function setTemplate(EngineInterface $templating)
     {
         $this->templating = $templating;
     }
-}
 
+    /**
+     * Get templating
+     * @return EngineInterface
+     */
+    public function getTemplate()
+    {
+        return $this->templating;
+    }
+}
--- src/TestLab/WebsiteBundle/Controller/IndexController.php
+++ src/TestLab/WebsiteBundle/Controller/IndexController.php
@@ -3,12 +3,18 @@
 namespace TestLab\WebsiteBundle\Controller;
 
 use TestLab\CommonBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-
+/**
+ * @Route(service="testlab.website.controller.index")
+ */
 class IndexController extends AbstractController
 {
+    /**
+     * @Route("/", name="test_lab_website_homepage")
+     */
     public function indexAction()
     {
-        return $this->templating->renderResponse('TestLabWebsiteBundle:Index:index.html.twig');
+        return $this->getTemplate()->renderResponse('TestLabWebsiteBundle:Index:index.html.twig');
     }
 }
