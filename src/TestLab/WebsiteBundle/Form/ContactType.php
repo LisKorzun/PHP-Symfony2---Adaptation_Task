7dd3b07b1d889d3cba641098141f7793bd1b57ea
Olesya Korzun <liskorzun@gmail.com>
2015-03-30 02:25:57 +0300

    Add validation

--- src/TestLab/WebsiteBundle/Form/ContactType.php
+++ src/TestLab/WebsiteBundle/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace TestLab\WebsiteBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class ContactType extends AbstractType
 {
@@ -18,22 +19,49 @@ class ContactType extends AbstractType
                 'label' => 'Name:',
                 'attr' => array(
                     'class' => 'form-control'
-                )))
+                ),
+                'constraints' => array(
+                    new Assert\NotBlank(array('message' => 'Name is required')),
+                    new Assert\Length(array('min' => 3, 'max' => 50)),
+                    new Assert\Regex(array(
+                        'pattern'     => '/^[a-zA-Z0-9_]+$/i',
+                        'message' => 'Your name can contain numbers, letters and _'
+                    ))
+                ),
+            ))
             ->add('email', 'email', array(
                 'label' => 'Email:',
                 'attr' => array(
                     'class' => 'form-control'
-                )))
+                ),
+                'constraints' => array(
+                    new Assert\NotBlank(array('message' => 'Email is required')),
+                    new Assert\Email(array(
+                        'message' => 'The email {{ value }} is not a valid email.',
+                        'checkMX' => true,
+                    ))
+                ),
+            ))
             ->add('subject', 'text', array(
                 'label' => 'Subject:',
                 'attr' => array(
                     'class' => 'form-control'
-                )))
+                ),
+                'constraints' => array(
+                    new Assert\NotBlank(array('message' => 'Subject is required')),
+                    new Assert\Length(array('min' => 3, 'max' => 200)),
+                ),
+            ))
             ->add('body', 'textarea', array(
                 'label' => 'Body:',
                 'attr' => array(
                     'class' => 'form-control'
-                )))
+                ),
+                'constraints' => array(
+                    new Assert\NotBlank(array('message' => 'Body is required')),
+                    new Assert\Length(array('min' => 5, 'max' => 1000)),
+                ),
+            ))
         ;
     }
 
